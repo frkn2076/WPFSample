0aadd96a6c1f7d85bcf1e7b03acd687221c3e0b1
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2020-08-26 23:29:22 +0300

    Left click disabled

--- SomeSample/MainWindow.xaml.cs
+++ SomeSample/MainWindow.xaml.cs
@@ -35,17 +35,17 @@ namespace SomeSample {
                 MyEllipse.Margin = new Thickness(MyEllipse.Margin.Left - 10, MyEllipse.Margin.Top - 10, 0, 0);
         }
 
-        private void Window_MouseLeftButtonDown(object sender, EventArgs e) {
-            var ellipse = new Ellipse();
-            ellipse.Fill = Brushes.Brown;
-            ellipse.Height = 40;
-            ellipse.Width = 40;
-            ellipse.Stroke = Brushes.Black;
-            var left = new Random().Next(0, 1920);
-            var top = new Random().Next(0, 1080);
-            ellipse.Margin = new Thickness(0, 0, left, top);
-            MyGrid.Children.Add(ellipse);
-        }
+        //private void Window_MouseLeftButtonDown(object sender, EventArgs e) {
+        //    var ellipse = new Ellipse();
+        //    ellipse.Fill = Brushes.Brown;
+        //    ellipse.Height = 40;
+        //    ellipse.Width = 40;
+        //    ellipse.Stroke = Brushes.Black;
+        //    var left = new Random().Next(0, 1920);
+        //    var top = new Random().Next(0, 1080);
+        //    ellipse.Margin = new Thickness(0, 0, left, top);
+        //    MyGrid.Children.Add(ellipse);
+        //}
 
         private void Window_MouseRightButtonDown(object sender, MouseButtonEventArgs e) {
             Application.Current.Shutdown();
